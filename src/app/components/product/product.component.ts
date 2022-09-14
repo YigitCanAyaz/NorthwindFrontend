95f4c9ddfdca99e241ad889b50866f8abeafc074
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-09-14 23:08:48 +0300

    integrated products/getall backend api to product

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from './../../models/productResponseModel';
 import { Product } from './../../models/product';
 import { Component, OnInit } from '@angular/core';
 
@@ -6,13 +8,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
+
 export class ProductComponent implements OnInit {
 
   products: Product[] = [];
 
-  constructor() { }
+  apiUrl = 'https://localhost:44319/api/products/getall';
+
+  // productResponseModel: ProductResponseModel = {
+  //   data: this.products,
+  //   message: "",
+  //   success: true
+  // };
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    // gelen datayı, ProductResponseModel'e map eder
+    // Aşağıdaki komut satırını yazdığımızda hiçbir anlamı yok çünkü asenkron çalışma durumu ve bunu çözmek için Angular ekibi Observable Design Pattern kullanıyor
+    //this.httpClient.get<ProductResponseModel>(this.apiUrl);
+
+    this.httpClient.get<ProductResponseModel>(this.apiUrl).subscribe(response => { this.products = response.data });
   }
 
 }
