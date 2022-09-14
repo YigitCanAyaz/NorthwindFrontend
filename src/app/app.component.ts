a1dc62bd006aa28a64b9c07b45fd3ba14910c926
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-09-14 18:04:39 +0300

    structure of nortwhind frontend application + added components for category, navi and product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+// Component olduğunu burada belirtiyoruz
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'northwindt';
+  title: string = 'northwind';
+  user: string = 'Engin Demiroğ';
 }
