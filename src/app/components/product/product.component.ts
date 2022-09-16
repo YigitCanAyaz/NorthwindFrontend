8537ac3624bf9b6e364905977ce4f781b54f70d1
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-09-17 02:00:10 +0300

    new feature (dynamic category menu for product)

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,7 @@
 import { ProductService } from './../../services/product.service';
 import { Product } from './../../models/product';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -19,14 +20,27 @@ export class ProductComponent implements OnInit {
   //   success: true
   // };
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.getProducts();
+    this.activatedRoute.params.subscribe(params => {
+      if (params["categoryId"]) {
+        this.getProductsByCategory(params["categoryId"]);
+      } else {
+        this.getProducts();
+      }
+    })
   }
 
   getProducts(): void {
     this.productService.getProducts().subscribe(response => {
+      this.products = response.data
+      this.dataLoaded = true;
+    });
+  }
+
+  getProductsByCategory(categoryId: number): void {
+    this.productService.getProductsByCategory(categoryId).subscribe(response => {
       this.products = response.data
       this.dataLoaded = true;
     });
