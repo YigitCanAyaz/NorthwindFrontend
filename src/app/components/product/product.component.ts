d84a0e76578b2c7a726b94427276cad5a7852c96
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-09-19 01:21:52 +0300

    control check for productId 1

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -49,7 +49,11 @@ export class ProductComponent implements OnInit {
   }
 
   addToCart(product: Product): void {
-    this.toastrService.success("Sepete eklendi", product.productName);
+    if (product.productId === 1) {
+      this.toastrService.error("Hata", "Bu ürün sepete eklenemez");
+    } else {
+      this.toastrService.success("Sepete eklendi", product.productName);
+    }
   }
 
 }
