0a357f37909719e3e466030496fef1c81d8ca646
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-09-21 22:51:09 +0300

    created login operation for auth.service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LoginModel } from '../models/loginModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  apiUrl = 'https://localhost:44319/api/auth';
+
+  constructor(private httpClient: HttpClient) { }
+
+  login(loginModel: LoginModel) {
+    return this.httpClient.post(this.apiUrl + "login", loginModel);
+  }
 }
